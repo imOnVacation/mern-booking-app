3d0e45c566ca7c112573c982b544f809c3065309
Leshi Chen <leshi@pdx.edu>
2024-03-22 12:50:49 -0700

    added signin functionality to the page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useForm } from 'react-hook-form';
+import * as apiClient from '../api-client';
+import { useMutation } from 'react-query';
 
 export type SignInFormData = {
   email: string;
@@ -9,9 +11,26 @@ const SignIn = () => {
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm<SignInFormData>();
+
+  const mutation = useMutation(apiClient.signIn, {
+    onSuccess: async () => {
+      console.log('user has been signed in');
+      // 1. show the toast
+      // 2. navigate to the home page
+    },
+    onError: (error: Error) => {
+      // show the toast
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className='flex flex-col gap-5'>
+    <form className='flex flex-col gap-5' onSubmit={onSubmit}>
       <h2 className='text-3xl font-bold'>Sign In</h2>
       <label className='text-gray-700 text-sm font-bold flex-1'>
         Email
@@ -41,6 +60,14 @@ const SignIn = () => {
           <span className='text-red-500'>{errors.password.message}</span>
         )}
       </label>
+      <span>
+        <button
+          type='submit'
+          className='bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl'
+        >
+          Login
+        </button>
+      </span>
     </form>
   );
 };
