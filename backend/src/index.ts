6bd16211f5867a45862347ec8ee80aed5bd7ced9
Leshi Chen <leshi@pdx.edu>
2024-03-23 12:39:46 -0700

    adding mongodb connection to test e2e-test-db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,14 @@ import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() =>
+    console.log(
+      'Connected to database: ',
+      process.env.MONGODB_CONNECTION_STRING
+    )
+  );
 
 const app = express();
 app.use(cookieParser());
