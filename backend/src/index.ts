5f7998d544fcd038da4e3a99069652f8583cd96b
Leshi Chen <leshi@pdx.edu>
2024-03-24 13:39:11 -0700

    since e2e test db is able to connect, remove the test message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,14 +6,7 @@ import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
 
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() =>
-    console.log(
-      'Connected to database: ',
-      process.env.MONGODB_CONNECTION_STRING
-    )
-  );
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(cookieParser());
