12c8e8ed34fc139b0a1d9a491b4993c691870101
Leshi Chen <leshi@pdx.edu>
2024-03-24 14:24:20 -0700

    aded test for registration feature

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -20,3 +20,30 @@ test('should allow the user to sign in', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'my Hotels' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Sign Out' })).toBeVisible();
 });
+
+test('should allow user to register', async ({ page }) => {
+  const testEmail = `test_register_${
+    Math.floor(Math.random() * 90000) + 10000
+  }@test.com`;
+
+  await page.goto(UI_URL);
+
+  await page.getByRole('link', { name: 'Sign In' }).click();
+  await page.getByRole('link', { name: 'Create an account here' }).click();
+  await expect(
+    page.getByRole('heading', { name: 'Create an Account' })
+  ).toBeVisible();
+
+  await page.locator('[name=firstName]').fill('test_firstName');
+  await page.locator('[name=lastName]').fill('test_lastname');
+  await page.locator('[name=email]').fill(testEmail);
+  await page.locator('[name=password]').fill('password');
+  await page.locator('[name=confirmPassword]').fill('password');
+
+  await page.getByRole('button', { name: 'Create Account' }).click();
+
+  await expect(page.getByText('Registration Success!')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'my Bookings' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'my Hotels' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sign Out' })).toBeVisible();
+});
